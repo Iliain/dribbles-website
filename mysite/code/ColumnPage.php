23c077e06f913c4280869f5d9177cdfdc1f5fad0
drew-ctrack <drew@ctrack.com.au>
2018-06-10 21:36:18 +1000

    column page

--- mysite/code/ColumnPage.php
+++ mysite/code/ColumnPage.php
@@ -10,13 +10,13 @@ class ColumnPage extends Page
     private static $has_one = [];
 
     private static $has_many = [
-      "Characters" => Character::class
+      //"Characters" => Character::class
     ];
 
     public function getCMSFields() {
         $fields = parent::getCMSFields();
-        $fields->addFieldToTab('Root.Characters', GridField::create('Characters', 'WoW Characters', $this->Characters(), GridFieldConfig_RecordEditor::create()));
-         return $fields;
+        //$fields->addFieldToTab('Root.Characters', GridField::create('Characters', 'WoW Characters', $this->Characters(), GridFieldConfig_RecordEditor::create()));
+        return $fields;
     }
 
 }
