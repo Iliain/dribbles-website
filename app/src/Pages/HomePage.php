08c56962ea1f9bc7419c379efc1eb13bdc6802cd
Drew Carrette <drew.carrette@internetrix.com.au>
2019-10-04 16:01:30 +1000

    Style committed

--- app/src/Pages/HomePage.php
+++ app/src/Pages/HomePage.php
@@ -1,12 +1,23 @@
 <?php
 
+use Dexven\FacebookFeed\Model\FacebookFeed;
+use SilverStripe\Forms\DropdownField;
+
 class HomePage extends Page
 {
+    private static $icon_class = 'font-icon-home';
+
     private static $db = [];
+
+    private static $has_one = [
+        'FacebookFeed' => FacebookFeed::class,
+    ];
 
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
+
+        $fields->addFieldToTab('Root.Main', DropdownField::create('FacebookFeedID', 'Select a feed:', FacebookFeed::get()->map('ID', 'Title')));
 
         return $fields;
     }
